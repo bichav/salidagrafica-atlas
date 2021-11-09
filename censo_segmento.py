1b3afc7e98acde238e9157055cab3baa16318fc2
Bichav <veronicaheredia9@gmail.com>
2021-11-09 15:26:14 -0300

    modifico ruta

--- censo_segmento.py
+++ censo_segmento.py
@@ -344,7 +344,7 @@ class CensoSegmento:
         ############################# Agrego la capa Descripcion ########################### 
         sql = aglomerado[0]
        # uri.setDataSource("", "( select * ,concat(prov,lpad(dpto::text,3,'0'),lpad(codloc::text,3,'0'),lpad(frac::text,2,'0'),lpad(radio::text,2,'0'),seg) link,  st_point(0,0) geom from indec.describe_segmentos_con_direcciones('" + sql + "'))","geom","", "segmento_id")
-        uri.setDataSource('', " ( select * ,concat(prov,lpad(dpto::text,3,'0'),lpad(codloc::text,3,'0'),lpad(frac::text,2,'0'),lpad(radio::text,2,'0'),seg) link,  st_point(0,0) geom from " + sql + '.r3'")","geom","", "segmento_id")
+        uri.setDataSource('', " ( select * ,concat(prov,lpad(dpto::text,3,'0'),lpad(codloc::text,3,'0'),lpad(frac::text,2,'0'),lpad(radio::text,2,'0'),seg) link,  st_point(0,0) geom from " + sql + ".r3)","geom","", "segmento_id")
         layer = QgsVectorLayer(uri.uri(), "descripcion", "postgres")
         if not layer.isValid():
             print ("No se cargo capa Descripcion")
@@ -543,7 +543,7 @@ class CensoSegmento:
         ############################# Agrego la capa Descripcion ########################### 
         sql = aglomerado[0] 
         #uri.setDataSource("", "( select * ,concat(prov,lpad(dpto::text,3,'0'),lpad(codloc::text,3,'0'),lpad(frac::text,2,'0'),lpad(radio::text,2,'0'),seg) link,  st_point(0,0) geom from indec.describe_segmentos_con_direcciones('" + sql + "'))","geom","", "segmento_id")
-        uri.setDataSource('', " ( select * ,concat(prov,lpad(dpto::text,3,'0'),lpad(codloc::text,3,'0'),lpad(frac::text,2,'0'),lpad(radio::text,2,'0'),seg) link,  st_point(0,0) geom from " + sql + '.r3'")","geom","", "segmento_id")
+        uri.setDataSource('', " ( select * ,concat(prov,lpad(dpto::text,3,'0'),lpad(codloc::text,3,'0'),lpad(frac::text,2,'0'),lpad(radio::text,2,'0'),seg) link,  st_point(0,0) geom from " + sql + ".r3)","geom","", "segmento_id")
         layer = QgsVectorLayer(uri.uri(), "descripcion_seg", "postgres")
         if not layer.isValid():
             print ("No se cargo capa Descripcion")
