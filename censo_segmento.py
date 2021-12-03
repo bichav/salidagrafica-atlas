7c9e8b46da2151ac88f940a927c8d5bef61195c3
Bichav <veronicaheredia9@gmail.com>
2021-12-03 10:13:28 -0300

    se modifico conexion

--- censo_segmento.py
+++ censo_segmento.py
@@ -222,10 +222,11 @@ class CensoSegmento:
         ##########Conexion desde BD a Postgis
         QgsProject.instance().clear()
         qs = QSettings()
-        dbHost = qs.value("PostgreSQL/connections/informatica/host",'10.70.80.62')
+
+        dbHost = qs.value("PostgreSQL/connections/informatica/host",'172.26.68.222')
         dbPort = qs.value("PostgreSQL/connections/informatica/port",'5432')
-        dbName = qs.value("PostgreSQL/connections/informatica/database",'DEVSEG')
-        
+        dbName = qs.value("PostgreSQL/connections/informatica/database",'PRODSEG')
+
         ############Pedir al usuario cargar los campos de  usuario y contraseña
         dbUsr = QInputDialog.getText(None, 'usuario', 'Introduce el nombre de usuario de la base de datos')
         dbPwd = QInputDialog.getText(None, 'contraseña', 'Introduce la contraseña', QLineEdit.Password)
@@ -432,9 +433,11 @@ class CensoSegmento:
         ##########Conexion desde BD a Postgis
         QgsProject.instance().clear()
         qs = QSettings()
-        dbHost = qs.value("PostgreSQL/connections/informatica/host",'10.70.80.62')
+
+        dbHost = qs.value("PostgreSQL/connections/informatica/host",'172.26.68.222')
         dbPort = qs.value("PostgreSQL/connections/informatica/port",'5432')
-        dbName = qs.value("PostgreSQL/connections/informatica/database",'DEVSEG')
+        dbName = qs.value("PostgreSQL/connections/informatica/database",'PRODSEG')
+
         ############Pedir al usuario cargar los campos de  usuario y contraseña
         dbUsr = QInputDialog.getText(None, 'usuario', 'Introduce el nombre de usuario de la base de datos')
         dbPwd = QInputDialog.getText(None, 'contraseña', 'Introduce la contraseña', QLineEdit.Password)
